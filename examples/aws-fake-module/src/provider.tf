e1820a237706bf1a06fb5000e7db3e1cf1d46fdb
Silvio Silva <smsilva@gmail.com>
2021-10-29 07:17:09 -0300

    Adding AWS Provider - Test using azurerm as Terraform Backend

---
+++ examples/aws-fake-module/src/provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+}
+
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  backend "local" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
