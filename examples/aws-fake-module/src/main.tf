18f9a7dddbd78e8d151112c0fc1b8f1b6feff959
Silvio Silva <smsilva@gmail.com>
2021-10-29 21:26:05 -0300

    Refactor GCS Backend Template
    Fix plugin_cache_dir usage

--- examples/aws-fake-module/src/main.tf
+++ examples/aws-fake-module/src/main.tf
@@ -1,4 +1,20 @@
+resource "random_string" "storage_bucket_id" {
+  keepers = {
+    prefix = var.name
+  }
+
+  length      = 3
+  min_lower   = 1
+  min_numeric = 2
+  lower       = true
+  special     = false
+}
+
+locals {
+  storage_bucket_name = "${var.name}-${random_string.storage_bucket_id.result}"
+}
+
 resource "aws_s3_bucket" "default" {
-  bucket = var.name
+  bucket = local.storage_bucket_name
   acl    = "private"
 }
