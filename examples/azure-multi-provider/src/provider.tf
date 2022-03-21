1bf6f72c8f66e617a1a6126ff3448cc8dd4ece48
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-21 19:43:56 -0300

    feat(examples/azure-multi-provider/src/): Create new azure-multi-provider example

---
+++ examples/azure-multi-provider/src/provider.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "null" {
+}
+
+terraform {
+  required_version = ">= 0.15.0, < 2.0.0"
+
+  backend "local" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.74.0, < 3.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.1, < 4.0.0"
+    }
+  }
+}
