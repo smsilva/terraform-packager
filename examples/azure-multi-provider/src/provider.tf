32c51334846740fe06e822476422c96687fa673f
Silvio Silva <smsilva@gmail.com>
2022-04-15 11:37:30 -0300

    feat(examples/azure-multi-provider/src/provider.tf): Enable most recent provider version

--- examples/azure-multi-provider/src/provider.tf
+++ examples/azure-multi-provider/src/provider.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.74.0, < 3.0.0"
+      version = ">= 2.74.0, < 4.0.0"
     }
     null = {
       source  = "hashicorp/null"
