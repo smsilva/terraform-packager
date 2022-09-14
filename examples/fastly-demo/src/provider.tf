84a84c85abf930fbd1f4ee9f466fcfda0a5fd6fb
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2022-09-14 14:59:40 -0300

    fix(examples/fastly-demo/src/provider.tf): Update version restriction and provider block

--- examples/fastly-demo/src/provider.tf
+++ examples/fastly-demo/src/provider.tf
@@ -1,11 +1,14 @@
 terraform {
+  backend "local" {}
+
+  required_version = ">= 1.1.9, < 2.0.0"
+
   required_providers {
     fastly = {
       source  = "fastly/fastly"
-      version = ">= 2.0.0"
+      version = ">= 2.0.0, < 3.0.0"
     }
   }
-    required_version = ">= 1.1.9, < 2.0.0"
 }
 
 provider "fastly" {
