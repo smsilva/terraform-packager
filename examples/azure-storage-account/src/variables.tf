1c4d9c2bc12f3da0ac2694de7c00b1d30726b4d0
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2023-03-21 17:01:46 -0300

    feat: Improvements, -refreh-only/-refresh=true based on terraform version and help command

--- examples/azure-storage-account/src/variables.tf
+++ examples/azure-storage-account/src/variables.tf
@@ -1,7 +1,7 @@
 variable "name" {
   type        = string
   description = "Bucket Name (Storage Account). If set as 'generic-bucket-1' then you should get something like: generic-bucket-1-x5f"
-  default     = "waspfiles"
+  default     = "tfpackagerfiles"
 }
 
 variable "location" {
@@ -13,5 +13,5 @@ variable "location" {
 variable "resource_group_name" {
   type        = string
   description = "Azure Storage Account Resource Group Name"
-  default     = "wasp"
+  default     = "tfpackager-example-storage-account"
 }
